83c08bb726e08092d4aea69166aaa8f3dadcbeda
Martin Wang <jiahaow@ca.ibm.com>
2017-03-30 16:16:22 -0400

    Debugger implementation
    This commit contains both the frontend and the backend.
    
    The frontend contains a debug service, a slideout pane in the side bar
    (debug UI) and a deploy service.
    
    The backend talks to debug adapters which implement VSCode Debug
    Protocol. it can be used either as a module of Orionode or a standalone
    server.
    
    Change-Id: Ib6f17715f498301fa699bfad86a49ca6e667c7c9
    Signed-off-by: Martin Wang <jiahaow@ca.ibm.com>

--- modules/orionode/index.js
+++ modules/orionode/index.js
@@ -118,26 +118,6 @@ function startServer(options) {
 			app.use(require('./lib/orion_static')({ orionClientRoot: ORION_CLIENT, maxAge: options.maxAge, orionode_static: orionode_static, prependStaticAssets: prependStaticAssets, appendStaticAssets: appendStaticAssets}));
 		}
 
-		//error handling
-		app.use(function(err, req, res, next){
-			logger.error(req.originalUrl, err);
-			res.status(404);
-			// respond with html page
-//			if (req.accepts('html')) {
-//				res.render('404', { url: req.url });
-//				return;
-//			}
-
-			// respond with json
-			if (req.accepts('json')) {
-				res.send({ error: 'Not found' });
-				return;
-			}
-
-			// default to plain-text. send()
-			res.type('txt').send('Not found');
-		});
-
 		return app;
 	} catch (e) {
 		handleError(e);
